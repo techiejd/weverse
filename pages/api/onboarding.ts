1df05253154b08e3694f81a8bafa80eb0e141335
jimenez1917 <jimenez1917@gmail.com>
2022-10-11 16:30:22 -0500

    Uploader Component

--- pages/api/onboarding.ts
+++ pages/api/onboarding.ts
@@ -13,9 +13,6 @@ export default async function onboarding(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  //logger.info({body: req.body}, "Ayo this is wild");
-    //  logger.info({files: req.body["files"]}, "looking at files");
-
   const form = new formidable.IncomingForm({ multiples: true });
   const parsedForm = form.parse(req, async function (err, fields, files) {
     logger.info({error: err, files: files, fields: fields}, "info");
