0e81756394aee1c7248f06efa6408fc6c9d6f979
techiejd <62455039+techiejd@users.noreply.github.com>
2022-10-11 20:42:58 -0500

    Formatting and fixes when to display button

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -3,32 +3,34 @@ import styles from "../../styles/Home.module.css";
 import { useForm } from "react-hook-form";
 import { useState, MouseEvent, Component } from "react";
 import { useRouter } from "next/router";
-import FileUploader from '../../common/components/fileUpload'
+import FileUploader from "../../common/components/fileUpload";
 import { getUserSnapshot } from "../../common/db";
 import { userData, UserData } from "../../modules/db/schemas";
 import * as utils from "../../modules/facebook/messenger/utils";
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import Tooltip from '@mui/material/Tooltip';
-import React, { FC } from 'react';
-import {ButtonInput} from './buttonInput'
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import Tooltip from "@mui/material/Tooltip";
+import React, { FC } from "react";
+import { ButtonInput } from "./buttonInput";
 
 export async function getServerSideProps() {
   return getUserSnapshot(String(process.env.ADMIN_ID)).then(
     async (userSnapshot) => {
       return {
         props: {
-          admin: userData.parse(userSnapshot.data()) 
+          admin: userData.parse(userSnapshot.data()),
         },
       };
-    });
+    }
+  );
 }
 
-const dashboard: NextPage<{admin: UserData}> = (
-  props) => {
-  const [templatedMessage,setTemplatedMessage] = useState<string>('')
-  const [inputMessage,setInputMessage] = useState<string>('')
-  const [selectedFiles, setSelectedFiles] = useState<Array<{url: string, file: File}>>([]);
+const Dashboard: NextPage<{ admin: UserData }> = (props) => {
+  const [templatedMessage, setTemplatedMessage] = useState<string>("");
+  const [inputMessage, setInputMessage] = useState<string>("");
+  const [selectedFiles, setSelectedFiles] = useState<
+    Array<{ url: string; file: File }>
+  >([]);
   const {
     register,
     handleSubmit,
@@ -36,10 +38,10 @@ const dashboard: NextPage<{admin: UserData}> = (
   } = useForm();
   const router = useRouter();
   const onSubmit = async (data: any) => {
-    console.log(data)
+    console.log(data);
     const body = ((): FormData => {
       const body = new FormData();
-      body.append("message",data.message);
+      body.append("message", data.message);
       for (let i = 0; i < selectedFiles.length; i++) {
         body.append("messageFiles", selectedFiles[i].file);
       }
@@ -53,28 +55,28 @@ const dashboard: NextPage<{admin: UserData}> = (
     router.push("/admin/success");
     return true;
   };
-  const processInput = (e: MouseEvent)=>{
+  const processInput = (e: MouseEvent) => {
     e.preventDefault();
-    const templated = utils.Notify.templateBody(inputMessage,props.admin);
+    const templated = utils.Notify.templateBody(inputMessage, props.admin);
     setTemplatedMessage(templated);
-  }
+  };
 
-  const [buttonInputs, setButtonInputs] = useState<Array<() => JSX.Element>>([]);
+  const [buttonInputKeys, setButtonInputKeys] = useState<Array<number>>([]);
 
-  const incNumButtonInputs = (e:MouseEvent) => {
+  const incNumButtonInputs = (e: MouseEvent) => {
     e.preventDefault();
-    if(buttonInputs.length >= 3){
-      alert("max limit reached")
-    }else{
-      setButtonInputs([...buttonInputs,ButtonInput]);
+    if (buttonInputKeys.length >= 3) {
+      alert("max limit reached");
+    } else {
+      setButtonInputKeys([...buttonInputKeys, buttonInputKeys.length]);
     }
   };
 
-  const decNumButtonInputs = (e:MouseEvent) => {
+  const decNumButtonInputs = (e: MouseEvent) => {
     e.preventDefault();
-    if (buttonInputs.length > 0) {
-      buttonInputs.pop();
-      setButtonInputs([...buttonInputs, ButtonInput])
+    if (buttonInputKeys.length > 0) {
+      buttonInputKeys.pop();
+      setButtonInputKeys([...buttonInputKeys]);
     }
   };
 
@@ -82,7 +84,7 @@ const dashboard: NextPage<{admin: UserData}> = (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Actuar como Sofí : {props.admin.name}</h1>
-       <div>
+        <div>
           <form action="/api/onboarding" onSubmit={handleSubmit(onSubmit)}>
             <h2>Message:</h2>
             <textarea
@@ -90,38 +92,37 @@ const dashboard: NextPage<{admin: UserData}> = (
               className={styles.textInput}
               {...register("message", {
                 required: "*",
-                onChange: (e)=>setInputMessage(e.target.value)
+                onChange: (e) => setInputMessage(e.target.value),
               })}
             />
-            <br/>
-            <br/>
-               <FileUploader selectedFiles={selectedFiles} setSelectedFiles={setSelectedFiles} message="Activa tus publicaciones"/>
-            <br/>
-            <br/>
-              <button  onClick={decNumButtonInputs} >
-                  <RemoveIcon color="action"/>
-                </button>
-              <h1>Up to 3 buttons allowed</h1>
-              <button onClick={incNumButtonInputs}>
-                <AddIcon color="action" />
-              </button>
-              <br/>
-              <br/>
-            {buttonInputs.map((buttonInput) =>(
-              <ButtonInput/>
-            ))}
-            <button onClick={processInput}>
-              Check
+            <br />
+            <br />
+            <FileUploader
+              selectedFiles={selectedFiles}
+              setSelectedFiles={setSelectedFiles}
+              message="Activa tus publicaciones"
+            />
+            <br />
+            <br />
+            <button onClick={decNumButtonInputs}>
+              <RemoveIcon color="action" />
             </button>
-            {templatedMessage?(<>
-                {templatedMessage}
-            </>):(<></>)}
-            <br/>
-            <hr/>
+            <h1>Up to 3 buttons allowed</h1>
+            <button onClick={incNumButtonInputs}>
+              <AddIcon color="action" />
+            </button>
+            <br />
+            <br />
+            {buttonInputKeys.map((buttonInputKey) => (
+              <ButtonInput key={buttonInputKey} />
+            ))}
+            <button onClick={processInput}>Check</button>
+            {templatedMessage ? <>{templatedMessage}</> : <></>}
+            <br />
+            <hr />
             <button type="submit" className={styles.button}>
               Enviar
             </button>
-
           </form>
         </div>
       </main>
@@ -129,4 +130,4 @@ const dashboard: NextPage<{admin: UserData}> = (
   );
 };
 
-export default dashboard;
+export default Dashboard;
