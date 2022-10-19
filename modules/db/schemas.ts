bbc1846c8bbde66c6e6b979ab6f135034ee8b5e5
jimenez1917 <jimenez1917@gmail.com>
2022-10-19 13:47:41 -0500

    transactions WIP

--- modules/db/schemas.ts
+++ modules/db/schemas.ts
@@ -1,28 +1,28 @@
-import {z} from 'zod';
-import * as sofi from '../sofia/schemas';
+import { z } from "zod";
+import * as sofi from "../sofia/schemas";
 
 const resources = z.object({
-    [sofi.Resource.Ween]: z.number(),
-    [sofi.Resource.Camera]: z.number(),
-    [sofi.Resource.MagnifyingGlass]: z.number(),
-    [sofi.Resource.Mask]: z.number(),
-    [sofi.Resource.Star]: z.number(),
-    [sofi.Resource.VipPass]: z.number(),
+  [sofi.Resource.Ween]: z.number(),
+  [sofi.Resource.Camera]: z.number(),
+  [sofi.Resource.MagnifyingGlass]: z.number(),
+  [sofi.Resource.Mask]: z.number(),
+  [sofi.Resource.Star]: z.number(),
+  [sofi.Resource.VipPass]: z.number(),
 });
 
 export const userData = z.object({
-    name: z.string(),
-    psid: z.string(),
-    notifications_permissions: z.object({
-        expires: z.number(),
-        token: z.string(),
-    }),
+  name: z.string(),
+  psid: z.string(),
+  notifications_permissions: z.object({
+    expires: z.number(),
     token: z.string(),
-    gameInfo: z.object({
-        resources: resources
-    }),
-    asid: z.string(),
-})
+  }),
+  token: z.string(),
+  gameInfo: z.object({
+    resources: resources,
+  }),
+  asid: z.string(),
+});
 
 export type UserData = z.infer<typeof userData>;
 
@@ -30,38 +30,41 @@ export const resourceEnum = z.nativeEnum(sofi.Resource);
 
 export const changesInResources = resources.partial();
 
-export type ChangesInResources = z.infer<typeof changesInResources>
+export type ChangesInResources = z.infer<typeof changesInResources>;
 
 const baseUser = z.object({
-    name: z.string(),
-    id: z.string()
-})
+  name: z.string(),
+  id: z.string(),
+});
 const baseAdmin = z.object({
-    actingAsSofi: z.literal(true)
-})
+  actingAsSofi: z.literal(true),
+});
 
 const admin = baseAdmin.merge(baseUser.partial());
 type Admin = z.infer<typeof admin>;
-export const Sofi : Admin = {
-    actingAsSofi: true
-}
+export const Sofi: Admin = {
+  actingAsSofi: true,
+};
 
 const user = z.union([admin, baseUser]);
 
 // TODO(jddominguez): flesh out message
 const message = z.object({
-    text: z.string()
-})
+  text: z.string(),
+});
 
 const datum = z.discriminatedUnion("type", [
   z.object({ type: z.literal("message"), message: message }),
-  z.object({ type: z.literal("resourcesChange"), resourcesChange: changesInResources }),
-])
+  z.object({
+    type: z.literal("resourcesChange"),
+    resourcesChange: changesInResources,
+  }),
+]);
 
 export const transaction = z.object({
-    from: user,
-    to: user.array().nonempty(),
-    data: datum.array().nonempty()
-})
+  from: user,
+  to: user.array().nonempty(),
+  data: datum.array().nonempty(),
+});
 
 export type Transaction = z.infer<typeof transaction>;
--- pages/admin/transactions.tsx
+++ pages/admin/transactions.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import { Transaction } from "../../modules/db/schemas";
 // TODO(jimenez1917): Get correct styles
 import styles from "../../styles/Home.module.css";
+import adminStyles from "../../styles/Admin.module.css";
 
 // TODO(techiejd): Are these records or transactions?
 export async function getServerSideProps() {
@@ -11,7 +13,7 @@ export async function getServerSideProps() {
       to: [{ name: "David", id: "David's psid" }],
       data: [
         { type: "message", message: { text: "I'm a little teapot" } },
-        { type: "resourcesChange", resourcesChange: { "🌟": 5 } },
+        { type: "resourcesChange", resourcesChange: { "🌟": -5, "👺": 3 } },
       ],
     },
   ];
@@ -25,12 +27,58 @@ export async function getServerSideProps() {
 const Transactions: NextPage<{ transactions: Array<Transaction> }> = (
   props
 ) => {
+  const [isSofi, setIsSofi] = useState<Boolean>(
+    "actingAsSofi" in props.transactions[0].from
+  );
+  const [message, setMessage] = useState<string>("");
+  const [resourcesTransactions, setResourcesTransactions] = useState<Object>(
+    {}
+  );
+  useEffect(() => {
+    props.transactions[0].data.map((data, i) => {
+      if (data.type === "resourcesChange" && data.resourcesChange) {
+        setResourcesTransactions(data.resourcesChange);
+      } else if (data.type === "message") {
+        setMessage(data.message.text);
+      }
+    });
+  }, []);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Admin Transactions</h1>
-
-        <p className={styles.description}>Valida que el mensaje fue enviado</p>
+        <h1>Admin Transactions</h1>
+        <div>
+          {isSofi ? (
+            <div className={adminStyles.boxTransactions}>
+              <h1>From: Sofi → To: {props.transactions[0].to[0].name}</h1>
+              <h2>{message}</h2>
+              <ul>
+                {Object.entries(resourcesTransactions).map((resource, i) => (
+                  <li key={i}>
+                    {resource}
+                    <hr />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <div className={adminStyles.boxTransactions}>
+              <h1>
+                From: {props.transactions[0].from.name} → To:{" "}
+                {props.transactions[0].to[0].name}
+              </h1>
+              <h2>{message}</h2>
+              <ul>
+                {Object.entries(resourcesTransactions).map((resource, i) => (
+                  <li key={i}>
+                    {resource}
+                    <hr />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </main>
     </div>
   );
