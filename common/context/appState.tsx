7c87b663031b1a5676a7c0bc8f23ba92a43d45e1
techiejd <62455039+techiejd@users.noreply.github.com>
2023-03-13 16:05:11 -0500

    Switches to use compat App from firebase

--- common/context/appState.tsx
+++ common/context/appState.tsx
@@ -11,6 +11,7 @@ import {
   useContext,
   useState,
 } from "react";
+import { app } from "../utils/firebase";
 
 export type User = {
   phoneNumber: string;
@@ -24,17 +25,6 @@ export type AppState = {
 };
 
 const db = (() => {
-  const app = initializeApp({
-    apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_REACT_APP_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_REACT_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_REACT_APP_MEASUREMENT_ID,
-  });
-
   const db = getDatabase(app);
   return db;
 })();
