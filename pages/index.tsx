95932c21c10a6543d7edba03e7b24e204bf6b677
techiejd <62455039+techiejd@users.noreply.github.com>
2023-03-13 20:47:49 -0500

    Adds the non signed in ux for landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
-import { Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 
 const VotingPortal = () => {
-  return <Typography>Currently in construction</Typography>;
+  return (
+    <Box>
+      <Typography>
+        Hey! Sign in and register to participate in the OneWe Awards!
+      </Typography>
+      <Typography>
+        Or take a <Link href="/vote">sneek-peak</Link>.
+      </Typography>
+    </Box>
+  );
 };
 
 export default VotingPortal;
