e032bcdfbfcca4e68a54a9194af4f0c4a2ff7602
techiejd <62455039+techiejd@users.noreply.github.com>
2023-03-20 19:59:47 -0500

    Adds image uploader for organization

---
+++ modules/posi/fileInput.tsx
@@ -0,0 +1,90 @@
+import { Box, Typography, Input, CardMedia } from "@mui/material";
+import { Dispatch, SetStateAction, useState, ChangeEvent } from "react";
+
+function humanFileSize(size: number) {
+  var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+  return (
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    " " +
+    ["B", "kB", "MB", "GB", "TB"][i]
+  );
+}
+
+// Only supports single file atm
+const FileInput = ({
+  file,
+  setFile,
+  minFileSize,
+  maxFileSize,
+  accept,
+}: {
+  file: File | undefined;
+  setFile: Dispatch<SetStateAction<File | undefined>>;
+  minFileSize?: number;
+  maxFileSize?: number;
+  accept: "video" | "img";
+}) => {
+  const [error, setError] = useState("");
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setError("");
+    if (e.target.files) {
+      const fileSelectionWasCanceled = e.target.files.length == 0;
+      if (fileSelectionWasCanceled && file) {
+        setFile(undefined);
+        return;
+      }
+
+      const f = e.target.files[0];
+
+      if (
+        (minFileSize && f.size < minFileSize) ||
+        (maxFileSize && f.size > maxFileSize)
+      ) {
+        setFile(undefined);
+        e.target.value = "";
+        setError(
+          minFileSize && f.size < minFileSize
+            ? `File is too small. Must be greater than ${humanFileSize(
+                minFileSize
+              )}.`
+            : `File is too large. Must be smaller than ${humanFileSize(
+                maxFileSize ? maxFileSize : 0
+              )}.`
+        );
+      }
+
+      setFile(e.target.files[0]);
+    }
+  };
+
+  return (
+    <Box>
+      {error != "" && (
+        <Typography sx={{ color: "red" }}>Error: {error}</Typography>
+      )}
+      <Input
+        type="file"
+        onChange={handleFileChange}
+        slotProps={{
+          input: {
+            accept: accept == "video" ? "video/mp4" : "image/jpeg, image/jpg",
+          },
+        }}
+        error
+      />
+      {file && (
+        <CardMedia
+          sx={{
+            height: 200,
+            width: 200,
+          }}
+          component={accept}
+          image={URL.createObjectURL(file)}
+        />
+      )}
+    </Box>
+  );
+};
+
+export default FileInput;
--- pages/posi/upload.tsx
+++ pages/posi/upload.tsx
@@ -1,11 +1,12 @@
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import CitySearchInput from "../../modules/posi/CitySearchInput";
-import SingleVideoInput from "../../modules/posi/singleVideoInput";
 import TagsInput from "../../modules/posi/tagsInput";
+import FileInput from "../../modules/posi/fileInput";
 
 const PosiForm = () => {
   const [video, setVideo] = useState<File | undefined>();
+  const [img, setImg] = useState<File | undefined>();
 
   return (
     <form action="/api/posi">
@@ -18,13 +19,26 @@ const PosiForm = () => {
       />
       <TagsInput />
       <CitySearchInput />
-      <SingleVideoInput video={video} setVideo={setVideo} />
+      <FileInput
+        file={video}
+        setFile={setVideo}
+        minFileSize={1048576 /** 1MB */}
+        maxFileSize={2147483648 /** 2GB */}
+        accept={"video"}
+      />
       <TextField
         fullWidth
         label="Resumen (1000 caracteres)"
         name="summary"
         multiline
         inputProps={{ maxLength: 1000 }}
+      />
+      <Typography>Organization</Typography>
+      <FileInput
+        file={img}
+        setFile={setImg}
+        maxFileSize={1048576 /** 1MB */}
+        accept={"img"}
       />
       <Button variant="contained" sx={{ mt: 3 }} type="submit">
         Subir PoSI!
