1d0b5488d45331ecb105393850fa062bb8d194b5
techiejd <62455039+techiejd@users.noreply.github.com>
2023-03-21 10:48:14 -0500

    Changes colors to default for easier development

--- common/components/theme.tsx
+++ common/components/theme.tsx
@@ -2,13 +2,6 @@ import { LinkProps, createTheme } from "@mui/material";
 import { ReactNode, Ref, forwardRef } from "react";
 import Link from "next/link";
 
-const colors = {
-  accented: "#BB86FC",
-  foreground: "#525252",
-  background: "#121212",
-  text: "#FFFFFF",
-};
-
 const LinkBehavior = forwardRef(function LinkBehaviour(
   props: { href: URL; children: ReactNode },
   ref: Ref<HTMLAnchorElement> | undefined
@@ -29,7 +22,7 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           "&.Mui-selected": {
-            color: colors.accented,
+            color: "#FFFFFF",
           },
         },
       },
@@ -37,7 +30,7 @@ const theme = createTheme({
     MuiTabs: {
       styleOverrides: {
         indicator: {
-          backgroundColor: colors.accented,
+          backgroundColor: "#FFFFFF",
         },
       },
     },
@@ -50,22 +43,6 @@ const theme = createTheme({
       defaultProps: {
         LinkComponent: LinkBehavior,
       },
-    },
-  },
-  palette: {
-    primary: {
-      main: colors.background,
-    },
-    secondary: {
-      main: colors.accented,
-    },
-    background: {
-      default: colors.background,
-      paper: colors.foreground,
-    },
-    text: {
-      primary: colors.text,
-      secondary: colors.text,
     },
   },
 });
