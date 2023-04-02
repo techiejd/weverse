670a68eb330fd85d081ad03f9fcba61f9d1709ca
techiejd <62455039+techiejd@users.noreply.github.com>
2023-04-01 21:50:25 -0500

    Upgrades next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: false
+  }
 };
 
 module.exports = nextConfig;
