c3b09e2767fc10d8c3973ccb9cc555cf55501257
techiejd <62455039+techiejd@users.noreply.github.com>
2023-04-12 21:02:18 -0500

    Adds google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Layout from "../common/components/layout";
 import "../modules/auth/AuthCode.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,18 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>OneWe</title>
       </Head>
+      {/**<!-- Google tag (gtag.js) -->}*/}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NN708F3V4T"
+      ></Script>
+      <Script id="weverse-google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NN708F3V4T');`}
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
