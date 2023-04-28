6b42123b668dbb0e594d5ccd84aad8f4e374b8c9
techiejd <62455039+techiejd@users.noreply.github.com>
2023-04-28 15:52:47 -0500

    Adds maker page button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,97 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 import PageTitle from "../common/components/pageTitle";
+import AuthDialog from "../modules/auth/AuthDialog";
+import { useEffect, useState } from "react";
+import { AuthAction } from "../modules/auth/AuthDialog/context";
+import { AppState, useAppState } from "../common/context/appState";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { doc } from "firebase/firestore";
+import { memberConverter } from "../common/context/weverse";
+import { User } from "firebase/auth";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 
-const VotingPortal = () => {
+const MakerPortal = ({ appState }: { appState: AppState }) => {
+  const [user, loading, error] = useAuthState(appState.auth);
+  const [authDialogOpen, setAuthDialogOpen] = useState(false);
+  useEffect(() => {
+    if (user && !loading && !error) {
+    }
+  }, [user, loading, error, appState.firestore]);
+  const MakerPortalBtn = ({ user }: { user: User }) => {
+    const memberDocRef = doc(
+      appState.firestore,
+      "members",
+      user.uid
+    ).withConverter(memberConverter);
+    const [member, loading, error] = useDocumentData(memberDocRef);
+
+    return (
+      <Button
+        href={`/makers/${member?.makerId}`}
+        variant="contained"
+        disabled={!member}
+        fullWidth
+      >
+        📛 Mi Pagina Maker
+      </Button>
+    );
+  };
+
   return (
-    <Stack m={2} spacing={2}>
-      <PageTitle
-        title={
-          <>
-            🪐 <b>We</b>Verse
-          </>
-        }
+    <Box sx={{ width: "100%" }}>
+      <AuthDialog
+        open={authDialogOpen}
+        setOpen={setAuthDialogOpen}
+        initialAuthAction={AuthAction.register}
       />
-      <Button href="/posi" variant="contained">
-        Ver todos los impactos sociales.
-      </Button>
-      <Button href="/makers" variant="contained">
-        Ver a todos los Makers.
-      </Button>
-      <Button href="/posi/upload" variant="contained">
-        Sube tu impacto.
-      </Button>
-    </Stack>
+      {user ? (
+        <MakerPortalBtn user={user} />
+      ) : (
+        <Button
+          fullWidth
+          variant="contained"
+          onClick={() => {
+            setAuthDialogOpen(true);
+          }}
+        >
+          +🧑 : Registrarte como Maker.
+        </Button>
+      )}
+    </Box>
   );
 };
 
-export default VotingPortal;
+const WeVerse = () => {
+  const appState = useAppState();
+  return (
+    <Box>
+      <Stack m={2} spacing={2}>
+        <PageTitle
+          title={
+            <>
+              <b>One</b>We
+            </>
+          }
+        />
+        <Button href="/posi" variant="contained">
+          🤸‍♀️ Actions
+        </Button>
+        <Button href="/makers" variant="contained">
+          💪 Makers
+        </Button>
+        <Button href="/posi/upload" variant="contained">
+          +🤸‍♀️ : Agrega tu Action
+        </Button>
+        {appState ? (
+          <MakerPortal appState={appState} />
+        ) : (
+          <Button variant="contained" disabled>
+            Loading...
+          </Button>
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default WeVerse;
