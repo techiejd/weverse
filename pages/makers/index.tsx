ded83ca85bc1eec7237f3612e0d8d1b7a77c5f90
techiejd <62455039+techiejd@users.noreply.github.com>
2023-05-02 09:31:03 -0500

    Adding my maker portal WIP

--- pages/makers/index.tsx
+++ pages/makers/index.tsx
@@ -1,16 +1,25 @@
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collection } from "firebase/firestore";
 import { AppState, useAppState } from "../../common/context/appState";
-import { Box, CircularProgress, Stack, Typography } from "@mui/material";
+import { Box, CircularProgress, Fab, Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import MakerCard from "../../modules/makers/MakerCard";
 import { makerConverter } from "../../common/context/weverse";
 import PageTitle from "../../common/components/pageTitle";
+import { PlusOne } from "@mui/icons-material";
+import AuthDialog from "../../modules/auth/AuthDialog";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const MyMakerPortal = ({ appState }: { appState: AppState }) => {
+  const [user, loading, error] = useAuthState(appState.auth);
+};
 
 const MakersListed = ({ appState }: { appState: AppState }) => {
-  const [makersSnapshot, loading, error] = useCollection(
+  const [makersSnapshot, makersLoading, makersError] = useCollection(
     collection(appState.firestore, "makers").withConverter(makerConverter)
   );
+  const [authDialogOpen, setAuthDialogOpen] = useState(false);
+  //
 
   const [makers, setMakers] = useState<string[]>([]);
 
@@ -32,19 +41,39 @@ const MakersListed = ({ appState }: { appState: AppState }) => {
     });
   }, [makersSnapshot]);
   return (
-    <Stack
-      sx={{ alignItems: "center", justifyContent: "center", p: 1 }}
-      spacing={1}
-    >
-      <PageTitle title={<b>💪 Makers</b>} />
-      {error && (
-        <Typography color={"red"}>Error: {JSON.stringify(error)}</Typography>
-      )}
-      {loading && <Loading />}
-      {makers.map((maker) => (
-        <MakerCard makerId={maker} key={maker} />
-      ))}
-    </Stack>
+    <Box>
+      <Stack
+        sx={{ alignItems: "center", justifyContent: "center", p: 1 }}
+        spacing={1}
+      >
+        <PageTitle title={<b>💪 Makers</b>} />
+        {makersError && (
+          <Typography color={"red"}>
+            Error: {JSON.stringify(makersError)}
+          </Typography>
+        )}
+        {makersLoading && <Loading />}
+        {makers.map((maker) => (
+          <MakerCard makerId={maker} key={maker} />
+        ))}
+      </Stack>
+      <AuthDialog open={authDialogOpen} setOpen={setAuthDialogOpen} />
+      <Fab
+        variant="extended"
+        sx={{
+          position: "fixed",
+          bottom: 16,
+          right: 16,
+        }}
+        color="primary"
+        onClick={() => {
+          setAuthDialogOpen(true);
+        }}
+      >
+        <PlusOne sx={{ mr: 1 }} />
+        <Typography>¡Sumáte!</Typography>
+      </Fab>
+    </Box>
   );
 };
 
