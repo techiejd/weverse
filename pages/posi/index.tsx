e1af7af93d425f982d9fc8934bcccd9a64ad4489
techiejd <62455039+techiejd@users.noreply.github.com>
2023-06-27 14:22:59 -0500

    Reverts to using impact due to bug where the id isn't in the content and because testimonials need more work

--- pages/posi/index.tsx
+++ pages/posi/index.tsx
@@ -17,37 +17,38 @@ import {
 } from "firebase/firestore";
 import { useAppState } from "../../common/context/appState";
 import { PlusOne } from "@mui/icons-material";
-import { Content, PosiFormData, SocialProof } from "../../functions/shared/src";
-import { contentConverter } from "../../common/utils/firebase";
+import { PosiFormData } from "../../functions/shared/src";
+import { posiFormDataConverter } from "../../common/utils/firebase";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useCallback, useEffect, useState } from "react";
 import ImpactCard from "../../modules/posi/action/card";
-import SocialProofCard from "../../modules/posi/socialProofCard";
 
 const IndexPage = () => {
   const appState = useAppState();
   const [latestDoc, setLatestDoc] = useState<
-    QueryDocumentSnapshot<Content> | undefined
+    QueryDocumentSnapshot<PosiFormData> | undefined
   >(undefined);
-  const [content, setContent] = useState<Content[]>([]);
+  const [actions, setActions] = useState<PosiFormData[]>([]);
   const [hasMore, setHasMore] = useState(true);
   const batchSize = 3;
 
   useEffect(() => {
     let ignore = false;
     const firstQuery = query(
-      collection(appState.firestore, "content").withConverter(contentConverter),
+      collection(appState.firestore, "impacts").withConverter(
+        posiFormDataConverter
+      ),
       limit(batchSize),
       orderBy("createdAt", "desc")
     );
     getDocs(firstQuery).then((snap) => {
       if (!ignore) {
-        const latestContent = snap.docs.map((doc) => doc.data());
-        if (latestContent.length) {
+        const latestActions = snap.docs.map((doc) => doc.data());
+        if (latestActions.length) {
           setLatestDoc(snap.docs[snap.docs.length - 1]);
         }
-        setHasMore(latestContent.length == batchSize);
-        setContent((c) => [...c, ...latestContent]);
+        setHasMore(latestActions.length == batchSize);
+        setActions((actions) => [...actions, ...latestActions]);
       }
     });
     return () => {
@@ -60,42 +61,35 @@ const IndexPage = () => {
       return;
     }
     const nextQuery = query(
-      collection(appState.firestore, "content").withConverter(contentConverter),
+      collection(appState.firestore, "impacts").withConverter(
+        posiFormDataConverter
+      ),
       orderBy("createdAt", "desc"),
       startAfter(latestDoc),
       limit(batchSize)
     );
     getDocs(nextQuery).then((snap) => {
-      const latestContents = snap.docs.map((doc) => doc.data());
-      if (latestContents.length) {
+      const latestActions = snap.docs.map((doc) => doc.data());
+      if (latestActions.length) {
         setLatestDoc(snap.docs[snap.docs.length - 1]);
       }
-      setHasMore(latestContents.length == batchSize);
-      setContent((c) => [...c, ...latestContents]);
+      setHasMore(latestActions.length == batchSize);
+      setActions((actions) => [...actions, ...latestActions]);
     });
-  }, [setHasMore, setContent, latestDoc, setLatestDoc, appState.firestore]);
+  }, [appState.firestore, setHasMore, setActions, latestDoc, setLatestDoc]);
 
   return (
     <InfiniteScroll
       next={next}
       hasMore={hasMore}
       loader={<LinearProgress />}
-      dataLength={content.length}
+      dataLength={actions.length}
       style={{ display: "flex", flexDirection: "column" }}
     >
       <Grid container spacing={1} pl={1} pr={1}>
-        {content.map((c) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={c.id}>
-            {c.data &&
-              (c.type == "action" ? (
-                <ImpactCard posiData={c.data as PosiFormData} />
-              ) : (
-                <SocialProofCard
-                  socialProof={c.data as SocialProof}
-                  showAction
-                  showMaker
-                />
-              ))}
+        {actions.map((action) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={action.id}>
+            <ImpactCard posiData={action} />
           </Grid>
         ))}
       </Grid>
