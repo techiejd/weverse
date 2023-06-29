9e9eba5d64d895627be299bd744468564ac44e00
techiejd <62455039+techiejd@users.noreply.github.com>
2023-06-28 20:15:09 -0500

    Fix build

--- pages/makers/[makerId]/sponsor.tsx
+++ pages/makers/[makerId]/sponsor.tsx
@@ -1,7 +1,14 @@
+import { CircularProgress } from "@mui/material";
+import { useCurrentMaker } from "../../../modules/makers/context";
 import Sponsor from "../../../modules/makers/sponsor";
 
 const SponsorPage = () => {
-  return <Sponsor exitButtonBehavior={{ href: "/" }} />;
+  const [maker] = useCurrentMaker();
+  return maker ? (
+    <Sponsor exitButtonBehavior={{ href: "/" }} beneficiary={maker} />
+  ) : (
+    <CircularProgress />
+  );
 };
 
 export default SponsorPage;
