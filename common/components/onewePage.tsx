32c696db6dea151578205a9491faded7bdbb9c56
techiejd <62455039+techiejd@users.noreply.github.com>
2023-08-09 21:35:53 -0500

    Reversing some of the changes to see if it was really just mui

--- common/components/onewePage.tsx
+++ common/components/onewePage.tsx
@@ -1,20 +1,16 @@
-import { Box, CircularProgress } from "@mui/material";
+import { Box } from "@mui/material";
 import { Header } from "./header";
 import { ComponentType, FC } from "react";
-import { useMessages } from "next-intl";
 
 export const asOneWePage = <P extends object>(
   Component: ComponentType<P>
 ): FC<P> => {
   const OWP = (props: P) => {
-    const messages = useMessages();
     return (
-      (messages && (
-        <Box>
-          <Header />
-          <Component {...props} />
-        </Box>
-      )) || <CircularProgress />
+      <Box>
+        <Header />
+        <Component {...props} />
+      </Box>
     );
   };
   return OWP;
