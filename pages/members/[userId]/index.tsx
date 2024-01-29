1c3a87e237005b4b00f3bcc874832799b0c6a491
techiejd <62455039+techiejd@users.noreply.github.com>
2024-01-29 16:20:20 +0700

    Adds liked actions at bottom of member page

--- pages/members/[userId]/index.tsx
+++ pages/members/[userId]/index.tsx
@@ -1,10 +1,17 @@
-import { Button, Grid, Stack, Typography } from "@mui/material";
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useSignOut } from "react-firebase-hooks/auth";
 import { useTranslations } from "next-intl";
 import { asOneWePage } from "../../../common/components/onewePage";
 import { useAppState } from "../../../common/context/appState";
 import {
   fromCollectionId2PathAndType,
+  useAction,
   useCurrentInitiatives,
   useCurrentLikes,
   useCurrentTestimonials,
@@ -18,9 +25,15 @@ import InitiativeCard from "../../../modules/initiatives/InitiativeCard";
 import Sponsorships from "../../../modules/initiatives/sponsor/list";
 import { useCurrentSponsorships } from "../../../modules/members/context";
 import SocialProofCard from "../../../modules/posi/socialProofCard";
+import ImpactCard from "../../../modules/posi/action/card";
 
 export const getStaticPaths = CachePaths;
 export const getStaticProps = WithTranslationsStaticProps();
+
+const ImpactCardWrapper = ({ path }: { path: string }) => {
+  const [posi] = useAction(path);
+  return posi ? <ImpactCard posiData={posi} /> : <CircularProgress />;
+};
 
 const UserPage = asOneWePage(() => {
   // TODO(techiejd): Do admin story so that user page can be protected.
@@ -30,6 +43,7 @@ const UserPage = asOneWePage(() => {
     useCurrentInitiatives();
   const [testimonials] = useCurrentTestimonials();
   const [likedActionPaths] = useCurrentLikes();
+  console.log(likedActionPaths);
 
   const [signOut] = useSignOut(appState.auth);
   const [myMember] = useMyMember();
@@ -128,6 +142,13 @@ const UserPage = asOneWePage(() => {
         ))}
       </Grid>
       <Typography variant="h2">Your liked actions</Typography>
+      <Grid container spacing={1}>
+        {likedActionPaths?.map((l, idx) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={l}>
+            <ImpactCardWrapper path={l} />
+          </Grid>
+        ))}
+      </Grid>
     </Stack>
   );
 });
