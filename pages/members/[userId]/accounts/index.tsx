ce94a8519a90b981ac621fe885d835b76216bd13
techiejd <62455039+techiejd@users.noreply.github.com>
2024-02-07 09:16:20 +0700

    Quick fix to push to prod so we can test

--- pages/members/[userId]/accounts/index.tsx
+++ pages/members/[userId]/accounts/index.tsx
@@ -1,3 +1,9 @@
 // Should be able to see the accounts and connected initiatives for that user.
 // Controlled and uncontrolled accounts should be displayed in separate sections.
 // Should be able to see the balance for each account, regardless of whether it is controlled or uncontrolled.
+// TODO(techiejd): WIP.
+const Accounts = () => {
+  return <div>In progress</div>;
+};
+
+export default Accounts;
