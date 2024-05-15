4c83d71e0a3724a22829c8e42e37e212538f0990
techiejd <62455039+techiejd@users.noreply.github.com>
2024-05-15 17:27:28 +0700

    Switches to Image instead of img

--- app/[locale]/header/index.tsx
+++ app/[locale]/header/index.tsx
@@ -3,6 +3,7 @@ import DrawerButton from "./drawerButton";
 import LanguagePortal from "./languagePortal";
 import { locale, Locale } from "../../../functions/shared/src";
 import { getTranslations } from "next-intl/server";
+import Image from "next/image";
 
 const getLocalePortalMessages = async () => {
   const possibleLocales = Object.keys(locale.Enum) as Locale[];
@@ -23,12 +24,9 @@ const Header = async () => {
       <div className="self-stretch flex-1 flex flex-row items-center justify-between max-w-[85rem] gap-[1.25rem]">
         <DrawerButton>
           <div className="h-[1.875rem] rounded-lg overflow-hidden flex flex-row items-center justify-center py-[0.188rem] px-[0.5rem] box-border">
-            <img
-              className="h-[1.5rem] w-[1.5rem] relative overflow-hidden shrink-0"
-              loading="lazy"
-              alt=""
-              src="/menu.svg"
-            />
+            <div className="h-[1.5rem] w-[1.5rem] relative overflow-hidden shrink-0">
+              <Image alt="" src="/menu.svg" fill />
+            </div>
           </div>
           <div className="flex flex-row items-center justify-start py-[0rem] pr-[0.5rem] pl-[0rem]">
             <div className="relative text-[1rem] tracking-[-1px] leading-[1.875rem] font-medium font-bottom-nav-bar-label-text text-gray-700 text-left inline-block min-w-[2.5rem]">
@@ -37,39 +35,28 @@ const Header = async () => {
           </div>
         </DrawerButton>
         <button className="cursor-pointer [border:none] p-0 bg-[transparent] h-[1.794rem] w-[6.813rem] overflow-hidden shrink-0 flex flex-col items-center justify-between">
-          <img
-            className="self-stretch h-[1.794rem] relative max-w-full overflow-hidden shrink-0"
-            loading="lazy"
-            alt=""
-            src="/onewe-logo.svg"
-          />
+          <div className="self-stretch h-[1.794rem] relative max-w-full overflow-hidden shrink-0">
+            <Image alt="" src="/onewe-logo.svg" fill />
+          </div>
         </button>
         <div className="self-stretch flex flex-row items-center justify-start py-[0rem] pr-[0.5rem] pl-[0rem] gap-[0.25rem]">
           <LanguagePortal messages={JSON.stringify(languagePortalMessages)}>
-            <img
-              className="h-[1.125rem] w-[1.125rem] relative overflow-hidden shrink-0"
-              loading="lazy"
-              alt=""
-              src="/globe-icon.svg"
-            />
+            <div className="h-[1.35rem] w-[1.35rem] relative overflow-hidden shrink-0">
+              <Image alt="globe" src="/globe-icon.svg" fill />
+            </div>
             <div className="self-stretch flex flex-row items-center justify-start py-[0rem] pr-[0.125rem] pl-[0rem]">
               <div className="relative text-[1rem] tracking-[-1px] leading-[1.875rem] font-medium font-bottom-nav-bar-label-text text-gray-700 text-left inline-block min-w-[1.063rem]">
                 {l[0].toUpperCase() + l.slice(1)}
               </div>
-              <img
-                className="h-[0.925rem] w-[0.925rem] relative overflow-hidden shrink-0 z-[1] ml-[-0.125rem]"
-                alt=""
-                src="/chevrondown.svg"
-              />
+              <div className="h-[0.925rem] w-[0.925rem] relative overflow-hidden shrink-0 z-[1] ml-[-0.125rem]">
+                <Image alt="" src="/chevrondown.svg" fill />
+              </div>
             </div>
           </LanguagePortal>
           <button className="cursor-pointer [border:none] p-0 bg-[transparent] self-stretch w-[2.163rem] flex flex-row items-center justify-start">
-            <img
-              className="h-[1.35rem] w-[1.35rem] relative overflow-hidden shrink-0"
-              loading="lazy"
-              alt=""
-              src="/bell04.svg"
-            />
+            <div className="h-[1.35rem] w-[1.35rem] relative overflow-hidden shrink-0">
+              <Image alt="" src="/bell04.svg" fill />
+            </div>
             <div className="flex-1 flex flex-row items-center justify-start pt-[1.25rem] px-[0rem] pb-[0rem] z-[1] ml-[-0.625rem]">
               <div className="flex-1 rounded-71xl bg-international-red flex flex-col items-center justify-center p-[0.438rem] border-[1px] border-solid border-white">
                 <div className="w-[0.438rem] h-[0.563rem] relative text-[0.75rem] tracking-[-1px] leading-[1.875rem] font-medium font-bottom-nav-bar-label-text text-white text-center flex items-center justify-center shrink-0 min-w-[0.438rem]">
